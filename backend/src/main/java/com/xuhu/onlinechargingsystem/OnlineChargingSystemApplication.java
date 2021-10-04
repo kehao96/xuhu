0fdca4e04c51aba665d899222ff98ef5fa299a33
Kehao Hu <kehahu@amazon.com>
2021-10-03 22:57:38 -0700

    Set up Infrastructure

---
+++ backend/src/main/java/com/xuhu/onlinechargingsystem/OnlineChargingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.xuhu.onlinechargingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineChargingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineChargingSystemApplication.class, args);
+    }
+
+}
