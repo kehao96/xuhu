aa751d9657545719e680c009e14a6c573cdfdf83
Kehao Hu <kehahu@amazon.com>
2021-10-04 23:56:08 -0700

    Add Controller

--- backend/src/main/java/com/xuhu/onlinechargingsystem/controller/HomePageController.java
+++ backend/src/main/java/com/xuhu/onlinechargingsystem/controller/HomePageController.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class MainController {
+public class HomePageController {
     @GetMapping("/")
     public String home(){
-        return "home";
+        return "Home";
     }
 }
---
+++ backend/src/main/java/com/xuhu/onlinechargingsystem/domain/Customer.java
@@ -0,0 +1,13 @@
+package com.xuhu.onlinechargingsystem.domain;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class Customer {
+    private final String id;
+    private final String name;
+    private final String username;
+    private final String password;
+}
